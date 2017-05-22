8e156a9b0b6e46282685a00544a97eebe89b06c5
stoba <s-toba@rhizome-e.com>
2017-05-22 15:49:54 +0900

    Add basic login

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password'}}
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", logout_path, count: 1
+  end
+end
