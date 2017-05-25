777214acfeffe5aabc8df648bee61c2f165c36f9
Masaya Konishi <konishi@office-ore.net>
2017-05-25 09:52:15 +0900

    Userと関連のないメソッドをUserモデルに作らない
    変数名はわかりやすく、モデル名と同じ名前で複数形にして作る

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,4 @@ class User < ApplicationRecord
         BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-
-  # ユーザーのステータスフィードを返す
-  def feed
-    Report.all
-  end
 end
