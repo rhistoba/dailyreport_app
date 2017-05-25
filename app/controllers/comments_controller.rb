9b0f24d6cdf1b4d3b745ae1b2b8d33df978ec394
Masaya Konishi <konishi@office-ore.net>
2017-05-25 11:37:34 +0900

    リソースのネストをするように

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user, only: :destroy
+  before_action :set_report, only: %i(create destroy)
 
   def create
-    @comment = current_user.comments.build(comment_params)
+    # @comment = current_user.comments.build(comment_params)
+    # @comment.report_id = @report.id
+    @comment = @report.comments.build(comment_params)
+    @comment.user = current_user
     if @comment.save
       flash[:success] = "コメントしました"
       redirect_to report_url(@comment.report)
@@ -23,12 +27,16 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :report_id)
+    params.require(:comment).permit(:content)
   end
 
   # beforeアクション
   def correct_user
     @comment = current_user.comments.find_by(id: params[:id])
     redirect_to root_url if @comment.nil?
+  end
+
+  def set_report
+    @report = Report.find(params[:report_id])
   end
 end
