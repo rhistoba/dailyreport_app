f7ecb597be9983d16806619d4d9667307be96e65
stoba <s-toba@rhizome-e.com>
2017-05-25 13:35:04 +0900

    Userモデルから無関係のメソッドを削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   before_action :logged_in_user
 
   def home
-    @feed_items= current_user.feed.paginate(page: params[:page])
+    @reports = Report.paginate(page: params[:page])
   end
 end
