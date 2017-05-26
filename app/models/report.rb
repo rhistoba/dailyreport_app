8e6358fedf99eb57644097e64213fd9c2d2a4e53
stoba <s-toba@rhizome-e.com>
2017-05-26 11:12:07 +0900

    エラーメッセージのロケールを追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,6 +5,5 @@ class Report < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 1000 }
-  validates :date, presence: true, uniqueness: { scope: :user_id,
-    message: "１ユーザーが同じ日に複数日報を作成することはできません" }
+  validates :date, presence: true, uniqueness: { scope: :user_id }
 end
