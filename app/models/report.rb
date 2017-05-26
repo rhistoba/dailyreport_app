aa2b6579ee6870ba55736bf65ceb72b66e0048f0
stoba <s-toba@rhizome-e.com>
2017-05-26 13:42:09 +0900

    不要なコメントを削除

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  #default_scope -> { order(date: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 1000 }
