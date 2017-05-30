16af19c33bb36bfa27d0f58b095dbdd55a57c059
stoba <s-toba@rhizome-e.com>
2017-05-30 14:13:45 +0900

    カレンダービューの表示

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -4,6 +4,7 @@ class TopPageController < ApplicationController
 
   def home
     user_ids = User.working.select(:id)
-    @reports = Report.where(user_id: user_ids).order(date: :desc).paginate(page: params[:page])
+    @reports_user = Report.where(user: current_user)
+    @reports_all  = Report.where(user_id: user_ids).order(date: :desc).paginate(page: params[:page])
   end
 end
