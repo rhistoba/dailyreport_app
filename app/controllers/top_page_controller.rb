4076f32e85cd30362c56e159ca8ef59869c0909e
stoba <s-toba@rhizome-e.com>
2017-05-30 18:02:55 +0900

    トップページにユーザーの選択肢を追加

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -3,8 +3,8 @@ class TopPageController < ApplicationController
   before_action :confirm_retire
 
   def home
+    @users = User.working
     user_ids = User.working.select(:id)
-    @reports_user = Report.where(user: current_user)
-    @reports_all  = Report.where(user_id: user_ids).order(date: :desc).paginate(page: params[:page])
+    @reports  = Report.where(user_id: user_ids).order(date: :desc).paginate(page: params[:page])
   end
 end
