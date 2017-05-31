9c52b740cc7d7d0d2f0eb2ade0abac0fa700d662
stoba <s-toba@rhizome-e.com>
2017-05-31 17:19:39 +0900

    select_tagで選択されたユーザーに対する動きを修正、@calendar_userをidから取得するよう修正

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -2,7 +2,7 @@ class TopPageController < ApplicationController
 
   def home
     @users = User.working
-    @calendar_user = User.find_by(name: params[:calendar_user_name]) || current_user
+    @calendar_user = User.find_by(id: params[:calendar_user]) || current_user
     user_ids = User.working.select(:id)
     @reports  = Report.where(user_id: user_ids).order(date: :desc).paginate(page: params[:page])
   end
