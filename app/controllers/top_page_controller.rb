c0c58af00d6f010e0f9ef24837470dd526dc1dbf
stoba <s-toba@rhizome-e.com>
2017-05-31 17:35:27 +0900

    トップページの新着日報を10件だけ表示するよう変更

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -4,6 +4,6 @@ class TopPageController < ApplicationController
     @users = User.working
     @calendar_user = User.find_by(id: params[:calendar_user]) || current_user
     user_ids = User.working.select(:id)
-    @reports  = Report.where(user_id: user_ids).order(date: :desc).paginate(page: params[:page])
+    @reports  = Report.where(user_id: user_ids).order(date: :desc).limit(10)
   end
 end
