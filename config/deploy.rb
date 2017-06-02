59170a744755dc9763a90d33acd99d00b7e14f4b
stoba <s-toba@rhizome-e.com>
2017-06-02 11:19:56 +0900

    UnicornとCapistranoの設定ファイルの変更

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+# Multistage deployment extension
+set :application, 'your_site.com'
+set :repo_url, 'git@github.com:rhistoba/dailyreport_app.git'
+set :rbenv_ruby, '2.3.4'
+set :rbenv_type, :user
+set :deploy_to, "/var/www/#{fetch(:application)}"
+
+shared_path = "#{fetch(:deploy_to)}/shared"
+release_path = "#{fetch(:deploy_to)}/current"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :keep_releases, 10
+
+# Unicorn
+# unicorn.rb で指定した pid と一致するように。
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+set :unicorn_config_path, "#{release_path}/config/unicorn.rb"
+
+set :ssh_options, {
+    keys: %w(~/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey)
+}
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
---
+++ config/unicorn.rb
@@ -0,0 +1,51 @@
+# path
+app_path = '/var/www/your_site.com'
+current_path = "#{app_path}/current"
+shared_path = "#{app_path}/shared"
+
+working_directory current_path
+pid File.expand_path('tmp/pids/unicorn.pid', shared_path)
+listen File.expand_path('tmp/sockets/unicorn.sock', shared_path), :backlog => 64
+
+# logging
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
+# preload
+preload_app true
+
+# Number of processes
+worker_processes 4
+
+# Time-out
+timeout 30
+
+# use correct Gemfile on restarts
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
+end
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
